6d976a7a71627402418c965fe4e74d49f2bc7829
tarcio11 <tarcio_x@hotmail.com>
2021-06-10 17:47:56 -0300

    test: ensure MingLengthValidation return falsy if value is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -6,8 +6,15 @@ import faker from 'faker'
 describe('MinLengthValidation', () => {
   test('Should return error if value is invalid', () => {
     const field = faker.database.column()
-    const sut = new MinLengthValidation(field)
+    const sut = new MinLengthValidation(field, 5)
     const error = sut.validate({ [field]: faker.random.alphaNumeric(4) })
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy if value is valid', () => {
+    const field = faker.database.column()
+    const sut = new MinLengthValidation(field, 5)
+    const error = sut.validate({ [field]: faker.random.alphaNumeric(5) })
+    expect(error).toBeFalsy()
   })
 })
