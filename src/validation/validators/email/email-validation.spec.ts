52bdc69c6499588698da6dc1154c84608e4d3579
tarcio11 <tarcio_x@hotmail.com>
2021-06-10 17:29:44 -0300

    test: ensure EmailValidation return error if email is invalid

---
+++ src/validation/validators/email/email-validation.spec.ts
@@ -0,0 +1,13 @@
+import { EmailValidation } from './email-validation'
+import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
+
+import faker from 'faker'
+
+describe('EmailValidation', () => {
+  test('Should return error if email is invalid', () => {
+    const field = faker.database.column()
+    const sut = new EmailValidation(field)
+    const error = sut.validate({ [field]: faker.random.word() })
+    expect(error).toEqual(new InvalidFieldError())
+  })
+})
