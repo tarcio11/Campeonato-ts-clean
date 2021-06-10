242d9833fc9c2a2d87a013820532482503c42aa4
tarcio11 <tarcio_x@hotmail.com>
2021-06-10 17:52:17 -0300

    refactor: move SUT creation to a factory helper method

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,25 +1,27 @@
-import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
 import { MinLengthValidation } from './min-length-validation'
+import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
 
 import faker from 'faker'
+
+const makeSut = (field: string): MinLengthValidation => new MinLengthValidation(field, 5)
 
 describe('MinLengthValidation', () => {
   test('Should return error if value is invalid', () => {
     const field = faker.database.column()
-    const sut = new MinLengthValidation(field, 5)
+    const sut = makeSut(field)
     const error = sut.validate({ [field]: faker.random.alphaNumeric(4) })
     expect(error).toEqual(new InvalidFieldError())
   })
 
   test('Should return falsy if value is valid', () => {
     const field = faker.database.column()
-    const sut = new MinLengthValidation(field, 5)
+    const sut = makeSut(field)
     const error = sut.validate({ [field]: faker.random.alphaNumeric(5) })
     expect(error).toBeFalsy()
   })
 
   test('Should return falsy if field does not exists in schema', () => {
-    const sut = new MinLengthValidation('any_field', 5)
+    const sut = makeSut('any_field')
     const error = sut.validate({ invalidField: faker.random.alphaNumeric(5) })
     expect(error).toBeFalsy()
   })
