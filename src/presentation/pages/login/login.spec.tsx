e2220a95350732c5200b1a6e109b8bc900b3067c
tarcio11 <tarcio_x@hotmail.com>
2021-06-10 13:45:35 -0300

    feat: ensure Login not call Authentication if form is invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -106,4 +106,19 @@ describe('Login Component', () => {
       password
     })
   })
+
+  test('Should call Authentication only once', () => {
+    const { sut, authenticationSpy } = makeSut()
+    simulateValidSubmit(sut)
+    simulateValidSubmit(sut)
+    expect(authenticationSpy.callsCount).toBe(1)
+  })
+
+  test('Should not call Authentication if form is invalid', () => {
+    const validationError = faker.random.words()
+    const { sut, authenticationSpy } = makeSut({ validationError })
+    populateEmailField(sut)
+    fireEvent.submit(sut.getByTestId('submit'))
+    expect(authenticationSpy.callsCount).toBe(0)
+  })
 })
