ab923cedb2770d73a729185909b44bc8fb7d5d0e
tarcio11 <tarcio_x@hotmail.com>
2021-06-12 10:38:42 -0300

    feat: ensure SignUp show name error if Validation fails

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi'
 
 import Context from '@/presentation/contexts/form/form-contexts'
 import { Button, FormStatus, Input, LoginHeader } from '@/presentation/components'
 
 import { Container, Content, AnimationContainer } from './styles'
+import { Validation } from '@/presentation/protocols'
 
-const SignUp: React.FC = () => {
+type Props = {
+  validation: Validation
+}
+
+const SignUp: React.FC<Props> = ({ validation }) => {
+  const [state, setState] = useState({
+    isLoading: false,
+    name: '',
+    nameError: ''
+  })
+
+  useEffect(() => {
+    setState({
+      ...state,
+      nameError: validation.validate('name', { name: state.name })
+    })
+  }, [state.name])
+
   return (
     <Container>
       <Content>
         <AnimationContainer>
           <LoginHeader />
-          <Context.Provider value={{ state: {} }}>
+          <Context.Provider value={{ state, setState }}>
             <form>
               <h3>Faça seu Cadastro</h3>
               <Input icon={FiUser} name="name" placeholder="Nome" />
