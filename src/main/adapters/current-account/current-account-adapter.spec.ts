784bda46b3a6de08d42f182bae0979be58b06480
tarcio11 <tarcio_x@hotmail.com>
2021-06-12 17:44:14 -0300

    test: ensure getCurrentAccountAdapter calls localStorage.get with correct value

--- src/main/adapters/current-account/current-account-adapter.spec.ts
+++ src/main/adapters/current-account/current-account-adapter.spec.ts
@@ -1,4 +1,4 @@
-import { setCurrentAccountAdapter } from './current-account-adapter'
+import { getCurrentAccountAdapter, setCurrentAccountAdapter } from './current-account-adapter'
 import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
 import { mockAccountModel } from '@/domain/test'
 
@@ -8,5 +8,13 @@ describe('CurrentAccountAdapter', () => {
     const account = mockAccountModel()
     setCurrentAccountAdapter(account)
     expect(setSpy).toHaveBeenCalledWith('account', account)
+  })
+
+  test('Should call localStorage.get with correct value', () => {
+    const account = mockAccountModel()
+    const getSpy = jest.spyOn(LocalStorageAdapter.prototype, 'get').mockReturnValueOnce(account)
+    const result = getCurrentAccountAdapter()
+    expect(getSpy).toHaveBeenCalledWith('account')
+    expect(result).toEqual(account)
   })
 })
