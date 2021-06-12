15e1c7634d25b49a5c390eddb05f45ca698b8a1b
tarcio11 <tarcio_x@hotmail.com>
2021-06-12 11:54:37 -0300

    feat: ensure SignUp calls AddAccount only once

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -35,6 +35,10 @@ const SignUp: React.FC<Props> = ({ validation, addAccount }) => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
+    if (state.isLoading) {
+      return
+    }
+    setState({ ...state, isLoading: true })
     await addAccount.add({
       name: state.name,
       email: state.email,
