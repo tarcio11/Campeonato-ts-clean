dd7913f8632a43723cd265c33dae92d12fd46fd2
tarcio11 <tarcio_x@hotmail.com>
2021-06-12 10:39:30 -0300

    test: ensure SignUp show name error if Validation fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -1,19 +1,37 @@
 import React from 'react'
 import 'jest-localstorage-mock'
-import { render, RenderResult } from '@testing-library/react'
+import { fireEvent, render, RenderResult } from '@testing-library/react'
 import { SignUp } from '@/presentation/pages'
+import faker from 'faker'
+import { ValidationStub } from '@/presentation/test'
 
 type SutTypes = {
   sut: RenderResult
 }
 
-const makeSut = (): SutTypes => {
+type SutParams = {
+  validationError: string
+}
+
+const makeSut = (params?: SutParams): SutTypes => {
+  const validationStub = new ValidationStub()
+  validationStub.errorMessage = params?.validationError
   const sut = render(
-      <SignUp />
+      <SignUp validation={validationStub} />
   )
   return {
     sut
   }
+}
+
+export const testStatusForField = (sut: RenderResult, fieldName: string, validationError: string = ''): void => {
+  const field = sut.getByPlaceholderText(fieldName)
+  expect(field.title).toBe(validationError)
+}
+
+const populateField = (sut: RenderResult, fieldName: string, value = faker.random.word()): void => {
+  const input = sut.getByPlaceholderText(fieldName)
+  fireEvent.input(input, { target: { value } })
 }
 
 describe('Login Component', () => {
@@ -27,5 +45,12 @@ describe('Login Component', () => {
     expect(spinner.childElementCount).toBe(0)
     const submitButton = sut.getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBeTruthy()
+  })
+
+  test('Should show email error if Validation fails', () => {
+    const validationError = faker.random.words()
+    const { sut } = makeSut({ validationError })
+    populateField(sut, 'Nome')
+    testStatusForField(sut, 'Nome', validationError)
   })
 })
