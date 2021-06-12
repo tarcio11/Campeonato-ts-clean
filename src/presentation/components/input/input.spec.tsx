01241b175a3f2f2cf36d0d928f2801beb77ee0fa
tarcio11 <tarcio_x@hotmail.com>
2021-06-12 00:11:03 -0300

    test: ensure render highlight on input focus and blur

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import faker from 'faker'
-import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { Input } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-contexts'
 
@@ -16,7 +17,27 @@ describe('Input component', () => {
   test('should be able to render an input', () => {
     const field = faker.database.column()
     makeSut(field)
-    const inputWrap = screen.getByPlaceholderText('Email')
-    expect(inputWrap).toBeTruthy()
+    const inputElement = screen.getByPlaceholderText('Email')
+    expect(inputElement).toBeTruthy()
+  })
+
+  test('should render highlight on input focus and blur', async () => {
+    const field = faker.database.column()
+    makeSut(field)
+
+    const inputElement = screen.getByPlaceholderText('Email')
+    const inputWrap = screen.getByTestId('input-wrap')
+
+    fireEvent.focus(inputElement)
+
+    await waitFor(() => inputWrap)
+    expect(inputWrap).toHaveStyle('border-color: #41C88E')
+    expect(inputWrap).toHaveStyle('color: #41C88E')
+
+    fireEvent.blur(inputElement)
+
+    await waitFor(() => inputWrap)
+    expect(inputWrap).not.toHaveStyle('border-color: #41C88E')
+    expect(inputWrap).not.toHaveStyle('color: #41C88E')
   })
 })
